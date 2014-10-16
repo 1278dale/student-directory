f1877d47dd07a291ef06c4fefe6c17bfac04167c
Dale <dalestevens1278@gmail.com>
2014-10-16 22:02:08 +0100

    Updated list using {students.length}

--- directory.rb
+++ directory.rb
@@ -1,31 +1,55 @@
-student_count = 9
+# lets put all students into some form of an array
+students = [
+	"Alex Bease",
+	"Alan Bridger",
+	"Arfah",
+	"Ben Hutchinson",
+	"Claudia",
+	"Chris Handley",
+	"Christopher Batts",
+	"Vaidas Mykolaitis",
+	"Peter Saxton",
+	"Edward Byne",
+	"Giorgia",
+	"Henry Stanley",
+	"Izzy Markwick",
+	"Mihai Liviu Cojocar",
+	"Nick Dyer",
+	"Nick Dyer",
+	"Pavel Redics",
+	"Richard Lake",
+	"Roi Driscoll",
+	"Stephen Lloyd",
+	"Tom Groombridge",
+	"Attila",
+	"Anna Yanova"
+]
+# and then we print them.
 puts "The Students of my cohort at MA"
 puts "----------------"
-puts "Alex Bease"
-puts "Alan Bridger"
-puts "Arfah"
-puts "Ben Hutchinson"
-puts "Claudia"
-puts "Chris Handley"
-puts "Christopher Batts"
-puts "Vaidas Mykolaitis"
-puts "Peter Saxton"
-puts "Edward Byne"
-puts "Giorgia"
-puts "Henrey Stanley"
-puts "Izzy Markwick"
-puts "Mihai Liviu Cojocar"
-puts "Nick Dyer"
-puts "Pavel Redics"
-puts "Richard Lake"
-puts "Roi Driscoll"
-puts "Stephen Lloyd"
-puts "Tom Groombridge"
-puts "Attila"
-puts "Anna Yanova"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[11]
+puts students[12]
+puts students[13]
+puts students[14]
+puts students[15]
+puts students[16]
+puts students[17]
+puts students[18]
+puts students[19]
+puts students[20]
+puts students[21]
+puts students[22]
 #finally, we print the total number of students
-puts "Overall, we have #{student_count} great students"
-print "Overall, we have "
+puts "Overall, we have #{students.length} great students"
 #it's important to remember that print() doesn't add new line characters
-print student_count
-puts " great students"
