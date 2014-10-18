667ff8cacc4ff982a42033cdba474fe965871f51
Dale <dalestevens1278@gmail.com>
2014-10-18 18:22:16 +0100

    Refactored in the interactive menu, used case statements

--- directory.rb
+++ directory.rb
@@ -33,6 +33,39 @@ end
 students
 end
 
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+	print_header
+	print_students_list
+	print_footer
+end
+
+def process(selection)
+	case selection
+	when "1"
+		input_students
+	when "2"
+		show_students
+	when "9"
+		exit
+	else
+	puts "I don't know what you mean, try again please"
+	end
+end
+def interactive_menu
+	loop do
+		print_menu
+		process(gets.chomp)
+	end
+end
+
+interactive_menu
+
 students = input_students
 print_header
 print(students)
